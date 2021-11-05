b1e43bf8f375170b587b799de2a4f447d13874a2
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-05 16:59:26 +0200

    Add files via upload

---
+++ StringNameConverter/StringNameConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringNameConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string(Name): ");
+            String s = Console.ReadLine();
+            int k1 = s.IndexOf(" ");
+            String r1 = s.Substring(0, k1);
+            String s1 = s.Substring(k1 + 1);
+            int k2 = s1.IndexOf(" ");
+            String s2 = s1.Substring(k2 + 1);
+            String r = r1 + " " + s1[0] + ". " + s2;
+            Console.WriteLine(r);
+        }
+    }
+}
