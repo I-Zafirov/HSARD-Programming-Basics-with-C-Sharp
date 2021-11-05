78dc75f62ec59629b0ab76ebc4fdc618cdfd7a68
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-05 16:57:29 +0200

    Add files via upload

---
+++ StringLetterCounter/StringLetterCounter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StringLetterCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string: ");
+            String s = Console.ReadLine();
+            
+            int[] br = new int[26];
+            
+            for (int i = 0; i < 26; i++)
+                br[i] = 0;
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                int k = (int)s[i] - (int)'a';
+                br[k]++;
+            }
+            for (int i = 0; i < 26; i++)
+                Console.WriteLine("{0}:{1}", (char)(i + (int)'a'), br[i]);
+        }
+    }
+}
