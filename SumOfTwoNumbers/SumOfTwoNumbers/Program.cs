f25007f7d60601a63ed656e68c6b9b8796737f56
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-05 16:59:59 +0200

    Add files via upload

---
+++ SumOfTwoNumbers/SumOfTwoNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            Console.WriteLine("a+b = {0}", a + b);
+        }
+    }
+}
