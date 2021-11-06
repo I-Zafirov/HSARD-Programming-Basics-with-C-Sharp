d98e2e1dda36f1dede5acdc31ea9175f526d89bb
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-06 20:43:59 +0200

    Add files via upload

---
+++ NumbersInIntervalNToMDoWhile/NumbersInIntervalNToMDoWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumbersInIntervalNToMDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter n: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("enter m (m>n): ");
+            int m = int.Parse(Console.ReadLine());
+
+            int p = 1;
+
+            do
+            {
+                p = p * n;
+                n++;
+            }
+            while (n <= m);
+            Console.WriteLine("Произведение= {0}", p);
+        }
+    }
+}
