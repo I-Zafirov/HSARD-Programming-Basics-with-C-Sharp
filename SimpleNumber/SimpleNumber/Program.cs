c7983c46cd13c72f019107e3dc2db8154a1f01df
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-06 20:40:16 +0200

    Add files via upload

---
+++ SimpleNumber/SimpleNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimpleNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            int num = int.Parse(Console.ReadLine());
+            
+            bool prime = true;
+            int delitel = 2;
+            int maxdelitel = (int)Math.Sqrt(num);
+
+            while (prime && (delitel <= maxdelitel))
+            {
+                if (num % delitel == 0)
+                {
+                    prime = false;
+                    delitel++;
+                }
+                Console.WriteLine("It is prime number? {0}", prime);
+            }
+        }
+    }
+}
