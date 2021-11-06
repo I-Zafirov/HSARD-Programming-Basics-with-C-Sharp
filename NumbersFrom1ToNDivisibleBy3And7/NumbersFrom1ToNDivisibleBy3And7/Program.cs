501936809c4c84656ad52b06ccb7562d7570fdeb
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-06 21:19:13 +0200

    Add files via upload

---
+++ NumbersFrom1ToNDivisibleBy3And7/NumbersFrom1ToNDivisibleBy3And7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NumbersFrom1ToNDivisibleBy3And7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number N:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if ((i % 21) == 0)
+                {
+                    Console.WriteLine("Number= {0}", i);
+                }
+            }
+        }
+    }
+}
