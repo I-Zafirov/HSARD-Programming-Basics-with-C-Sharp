2e12d8cab86081e61f42633dc1bbbecbd6cd0e5e
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-06 22:35:55 +0200

    Add files via upload

---
+++ FirstNumDivide5SecondNumDivide2/FirstNumDivide5SecondNumDivide2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FirstNumDivide5SecondNumDivide2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Number=");
+            int num = int.Parse(Console.ReadLine());
+            
+            if ((num % 10) == 5)
+            {
+                Console.WriteLine("Цифрата на единиците се дели на 5");
+            }
+            else
+            {
+                Console.WriteLine("Цифрата на единиците не се дели на 5");
+            }
+            if (((num / 10) % 2) == 0)
+            {
+                Console.WriteLine("Цифрат на десетиците е четно число");
+            }
+            else
+            {
+                Console.WriteLine("Цифрите на десетиците е нечетно число");
+            }
+        }
+    }
+}
