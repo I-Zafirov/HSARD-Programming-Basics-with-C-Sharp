e97d04acbb12078de9596d33f678e90d9be4e94a
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-06 22:09:29 +0200

    Add files via upload

---
+++ SumOfNumbersFrom1To50DivisibleBy3And4/SumOfNumbersFrom1To50DivisibleBy3And4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumOfNumbersFrom1To50DivisibleBy3And4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 12 == 0)
+                    sum = sum + 1; //sum++
+            }
+            Console.WriteLine("Sum= {0}", sum);
+        }
+    }
+}
