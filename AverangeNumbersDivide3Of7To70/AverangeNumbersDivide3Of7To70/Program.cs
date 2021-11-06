c3d5c743ad7dc9c8973ea2096261fecea2373c21
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-06 20:52:44 +0200

    Add files via upload

---
+++ AverangeNumbersDivide3Of7To70/AverangeNumbersDivide3Of7To70/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AverangeNumbersDivide3Of7To70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int count = 0;
+            int aver = 0;
+            
+            for (int i = 7; i <= 70; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum = sum + 1;
+                    count++;
+                }
+            }
+            aver = sum / count;
+            Console.WriteLine("Averange={0}", aver);
+        }
+    }
+}
