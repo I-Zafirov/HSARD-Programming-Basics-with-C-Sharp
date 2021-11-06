0f547047ecffd31bf6a03f1c293e8a524da02750
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-06 20:32:39 +0200

    Add files via upload

---
+++ NextData/NextData/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace NextData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Data:");
+            int date = int.Parse(Console.ReadLine());
+            
+            int year = date % 10000;
+            int month = (date / 10000) % 100;
+            int day = (date / 10000) / 100;
+
+            switch (day)
+            {
+                case 28:
+                    {
+                        if (month == 2)
+                        {
+                            day = 1;
+                            month = 3;
+                            Console.WriteLine("{0} {1} {2}", day, month, year);
+                        }
+                        else
+                        {
+                            day++;
+                            Console.WriteLine("{0} {1} {2}", day, month, year);
+                        }
+                        break;
+
+                    }
+                case 30:
+                    {
+                        if (month == 4 || month == 6 || month == 9 || month == 11)
+                        {
+                            day = 1;
+                            month++;
+                            Console.WriteLine("{0} {1} {2}", day, month, year);
+                        }
+                        else
+                        {
+                            day++;
+                            Console.WriteLine("{0} {1} {2}", day, month, year);
+                        }
+                        break;
+                    }
+                case 31:
+                    {
+                        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10)
+                        {
+                            day = 1;
+                            month++;
+                            Console.WriteLine("{0} {1} {2}", day, month, year);
+                        }
+                        else if (month == 12)
+                        {
+                            day = 1;
+                            month = 1;
+                            year++;
+                            Console.WriteLine("{0} {1} {2}", day, month, year);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Data Error!");
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        day++;
+                        Console.WriteLine("{0} {1} {2}", day, month, year);
+
+                    }
+                    break;
+
+            }
+        }
+    }
+}
