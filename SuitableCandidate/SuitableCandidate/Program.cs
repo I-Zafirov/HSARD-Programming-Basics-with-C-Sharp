deb6af2174d35c4b3d7924d7d2a56784581fcd2d
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-06 20:49:59 +0200

    Add files via upload

---
+++ SuitableCandidate/SuitableCandidate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SuitableCandidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter gender (M or W): ");
+            string gender = (Console.ReadLine());
+            
+            int bank = 0;
+            int bust = 0;
+
+            if (gender == "M")
+            {
+                Console.WriteLine("Enter Bank availability: ");
+                bank = int.Parse(Console.ReadLine());
+            }
+            else if (gender == "W")
+            {
+                Console.WriteLine("Enter bust: ");
+                bust = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+            if (bank >= 250000 || bust >= 100)
+            {
+                Console.WriteLine("Suitable Candidate!");
+            }
+            else
+            {
+                Console.WriteLine("Unsuitable Candidate!");
+            }
+        }
+    }
+}
