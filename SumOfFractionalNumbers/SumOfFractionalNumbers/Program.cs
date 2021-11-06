ac295e70aaeb2bd3b83bb2d21eb3b55546317bf1
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-06 22:04:54 +0200

    Add files via upload

---
+++ SumOfFractionalNumbers/SumOfFractionalNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SumOfFractionalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0.0;
+            
+            for (int i = 2, j = 3; i <= 19; i++, j++)
+                sum = sum + (1 / (double)(i * j));
+            {
+                Console.WriteLine("Sum= {0}", sum);
+            }
+        }
+    }
+}
