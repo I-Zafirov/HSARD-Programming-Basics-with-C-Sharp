76b78095bb8f710f3cd8bb6a7c35bbb83fe7baa0
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-11 22:51:53 +0200

    Add files via upload

---
+++ PayrollTax/PayrollTax/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PayrollTax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double z;
+            
+            Console.WriteLine("input Paymand");
+            z = Double.Parse(Console.ReadLine());
+            
+            if (z >= 650)
+            {
+                Console.WriteLine("Payroll tax:{0}BGN", z * 0.1);
+            }
+            else
+            {
+                Console.WriteLine("NO Payroll tax");
+            }
+        }
+    }
+}
