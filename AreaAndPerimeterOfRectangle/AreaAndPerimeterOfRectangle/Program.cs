91b804fd2faab3336da91a4767c1f77a08017255
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-11 22:49:51 +0200

    Add files via upload

---
+++ AreaAndPerimeterOfRectangle/AreaAndPerimeterOfRectangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AreaAndPerimeterOfRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double S;
+            double P;
+            
+            Console.WriteLine("input a");
+            a = Double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("input b");
+            b = Double.Parse(Console.ReadLine());
+           
+            Console.WriteLine("Area(S)={0}; Perimeter(P)={1}", S = (a * b), P = (2 * (a + b)));
+        }
+    }
+}
