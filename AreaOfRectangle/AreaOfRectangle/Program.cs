6627f9fcbf9f6c4d52cf3afcdc8c7961ba4a49e5
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-12 14:06:47 +0200

    Add files via upload

---
+++ AreaOfRectangle/AreaOfRectangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AreaOfRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0.0;
+            double b = 0.0;
+            double S = a * b;
+
+            Console.WriteLine("input a");
+            a = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("input b");
+            b = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("S={0}", S);
+        }
+    }
+}
