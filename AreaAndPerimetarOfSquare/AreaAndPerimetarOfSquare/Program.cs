b5cd29d0fcabb2eb7f98b03d6e72da874bd4dc7d
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-12 14:07:15 +0200

    Add files via upload

---
+++ AreaAndPerimetarOfSquare/AreaAndPerimetarOfSquare/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaAndPerimetarOfSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, S, P;
+
+            Console.WriteLine("input a");
+            a = Double.Parse(Console.ReadLine());
+
+            S = a * a;
+            P = 4 * a;
+
+            Console.WriteLine("Area = {0}; Perimetar = {1}", S, P);
+        }
+    }
+}
