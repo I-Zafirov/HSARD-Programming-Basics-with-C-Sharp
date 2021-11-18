8c1fca68eb962b1b745a4b910e8cbf2fc4b39838
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-18 19:35:05 +0200

    Add files via upload

---
+++ NextDayOfTheWeekEnum/NextDayOfTheWeekEnum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NextDayOfTheWeekEnum
+{
+    class Program
+    {
+        enum Days {Mon = 1, Tue, Wed, Thu, Fri, Sat, Sun,};
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input 1-7:");
+            String s = Console.ReadLine();
+            byte d = Byte.Parse(s);
+
+            if (d == 7) d = 1;
+            else d++;
+
+            Days y = (Days)d;
+            
+            Console.WriteLine(y);
+        }
+    }
+}
