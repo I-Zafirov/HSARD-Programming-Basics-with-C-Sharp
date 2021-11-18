a42309608e3e79b969995f2cef525a337039dd4a
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-18 20:12:09 +0200

    Add files via upload

---
+++ ParameterTransmissionMechanism/ParameterTransmissionMechanism/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ParameterTransmissionMechanism
+{
+    class RefExample
+    {
+        static void Method(ref int i, int j)
+        {
+            i = i + 44;
+            j = j + 44;
+        }
+        static void Main(string[] args)
+        {
+            {
+                int val1 = 1;
+                int val2 = 1;
+                Method(ref val1, val2);     // val1 трябва да е инициализирана преди да се подаде
+                
+                Console.WriteLine("val1 = {0}; val2 = {1}", val1, val2);       // Output: val1=45; val2=1  
+            }
+
+        }
+    }
+}
