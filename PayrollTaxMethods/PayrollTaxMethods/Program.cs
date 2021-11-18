d407657b0b6b1f77d27261bedcba3b1fcaa8f786
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-18 20:07:54 +0200

    Add files via upload

---
+++ PayrollTaxMethods/PayrollTaxMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PayrollTaxMethods
+{
+    class Program
+    {
+        static public double dan(double z)
+        {
+            return z * 0.1;
+        }
+        static void Main(string[] args)
+        {
+            int P = 1;
+            for (int i = 1; i <= 100; i++)
+                P *= i;
+            Console.WriteLine("P = {0}", P);
+        }
+    }
+}
