8fa838d18d42045c2c0b40a1c19fcc82bf355ad2
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-19 15:35:37 +0200

    Add files via upload

---
+++ DailyFamilyExpensesPerWeek/DailyFamilyExpensesPerWeek/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DailyFamilyExpensesPerWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] M = new double[7];
+            Console.WriteLine("Input daily family expenses per week:");
+
+            for(int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Input expenses per day");
+                M[i] = double.Parse(Console.ReadLine());
+            }
+            double sum = 0.0d;
+
+            for(int i = 0; i < 7; i++)
+            {
+                sum += M[i];
+                Console.WriteLine("\n");
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
