ce4b8826ee8fa52355f5866dec7be41122eed302
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-19 20:21:26 +0200

    Add files via upload

---
+++ SumNumberArray/SumNumberArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumNumberArray
+{
+    class Program
+    {
+        static void input(int[]a, int n)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Input number:");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static void Sum(int[]b, int a)
+        {
+            int s = 0;
+
+            for(int i = 0; i < a; i++)
+            {
+                s += b[i];
+            }
+            Console.WriteLine(s);
+        }
+        static void Main(string[] args)
+        {
+            int[] c = new int[10];
+            input(c, 10);
+
+            Sum(c, 10);
+        }
+    }
+}
