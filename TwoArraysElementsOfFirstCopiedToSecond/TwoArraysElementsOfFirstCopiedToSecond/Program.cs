668fed32131e849decb71697474f9e7a943715d1
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-19 15:59:42 +0200

    Add files via upload

---
+++ TwoArraysElementsOfFirstCopiedToSecond/TwoArraysElementsOfFirstCopiedToSecond/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TwoArraysElementsOfFirstCopiedToSecond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] A = new int[5, 2] { { 0, 0 }, { 1, 2 }, { 2, 4 }, { 3, 6 }, { 4, 8 } };
+
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("[A{0},{1}] = {2}", i, j, A[i, j]);
+                }
+            }
+        }
+    }
+}
