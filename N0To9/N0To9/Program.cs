349095dbdc30dcf78c35c15ff2546cb1f4695da0
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-19 10:53:05 +0200

    Add files via upload

---
+++ N0To9/N0To9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace N0To9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[10];
+             for(int i = 0; i < 10; i++)
+            {
+                A[i] = i;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+        }
+    }
+}
