9eaa09dc3f5d5a878f8f1c2ca54c2eba95d93e3c
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-19 18:46:10 +0200

    Add files via upload

---
+++ TextInReverseOrder/TextInReverseOrder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TextInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input text:");
+            string text = Console.ReadLine();
+            string text1 = " ";
+
+            Console.WriteLine("Text in reverse order:");
+
+            for(int i = text.Length - 1; i >= 0; i--)
+            {
+                text1 += text[i];
+            }
+            Console.WriteLine(text1);
+        }
+    }
+}
