26e53c5bdda9e06a39c3d956d43f98e0c6b9fa3e
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-19 15:12:17 +0200

    Add files via upload

---
+++ HowManyTimesNIsContainedInTheSequence/HowManyTimesNIsContainedInTheSequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HowManyTimesNIsContainedInTheSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[100];
+            Console.WriteLine("Input number of elements (0 <= 100):");
+            int m = int.Parse(Console.ReadLine());
+
+            for(int k = 0; k < m; k++)
+            {
+                Console.WriteLine("Input element:");
+                numbers[k] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Input number N:");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for(int j = 0; j < m; j++)
+            {
+                if (numbers[j] == n)
+                    count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
