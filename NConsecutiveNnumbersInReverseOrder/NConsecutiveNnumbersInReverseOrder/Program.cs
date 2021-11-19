0dac7d918e74d79bf5d42ea23bf7807699c5ffd3
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-19 11:12:39 +0200

    Add files via upload

---
+++ NConsecutiveNnumbersInReverseOrder/NConsecutiveNnumbersInReverseOrder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NConsecutiveNnumbersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] M = new double[100];
+            Console.WriteLine("Input the number of elements:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Input element:");
+                A[i] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Reverse order is:");
+            Console.WriteLine("\n");
+
+            for (int i = n - 1; i >= 0; i--)
+
+            Console.WriteLine(A[i]);
+        }
+    }
+}
