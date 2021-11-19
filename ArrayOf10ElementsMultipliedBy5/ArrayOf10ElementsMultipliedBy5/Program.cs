8c51682b1abddad26dc1c737539b95f4b1447444
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-19 15:22:13 +0200

    Add files via upload

---
+++ ArrayOf10ElementsMultipliedBy5/ArrayOf10ElementsMultipliedBy5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayOf10ElementsMultipliedBy5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[10];
+            for(int i = 0; i < 10; i++)
+            {
+                A[i] = i * 5;
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+        }
+    }
+}
