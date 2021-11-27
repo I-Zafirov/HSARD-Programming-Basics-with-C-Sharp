49edb12ddc1df55c1920ae9dedf031a7aca6a6d8
I-Zafirov <86560208+I-Zafirov@users.noreply.github.com>
2021-11-27 15:40:49 +0200

    Add files via upload

---
+++ BookSearchPublicStruct/BookSearchPublicStruct/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BookSearchPublicStruct
+{
+    class Program
+    {
+        public struct Book
+        {
+            public string Name;
+            public string Author;
+            public int Year;
+            public double Price;
+        };
+        static void BookIn(Book[] b)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Input Book Name:");
+                b[i].Name = Console.ReadLine();
+
+                Console.WriteLine("Input Book Author:");
+                b[i].Author = Console.ReadLine();
+
+                Console.WriteLine("Input Book Year:");
+                b[i].Year = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Input Book Price:");
+                b[i].Price = double.Parse(Console.ReadLine());
+            }
+        }
+        static void Names(Book[] b)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (b[i].Year > 2005)
+                {
+                    Console.WriteLine(b[i].Name, b[i].Author, b[i].Year, b[i].Price);
+                }
+                else
+                    Console.WriteLine("Not existing book!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Book[] b = new Book[10];
+            BookIn(b);
+            Names(b);
+        }
+    }
+}
